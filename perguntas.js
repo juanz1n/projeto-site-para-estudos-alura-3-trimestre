ddd6b2ef596543bd3fda25d8d560c57951735414
juazinxzy <141247580+juanz1n@users.noreply.github.com>
2024-11-20 20:49:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é PHP?',
+    'O PHP é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da India?',
+    'A capital da India é Nova Delhi'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma classes e objetos?',
+    'fundamentais na programação orientada a objetos (POO). Classes são como "modelos" que definem propriedades e comportamentos, enquanto objetos são instâncias dessas classes.'
+)
+
+criaCartao(
+    'Lingua Hindi',
+    'Como se diz Bom dia em Hindi?',
+    'Bom dia em Hindi é "सुप्रभात" (pronuncia-se "su-prab-hat"). '
+)
